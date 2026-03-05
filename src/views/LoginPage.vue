a0b6c7682e6843e94a327d257f42233e4bef2c14
phanhuyhieu2004 <hieuhuyphantn@gmail.com>
2026-03-05 11:38:05 +0700

    Update API connection and Google login URL

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -63,7 +63,7 @@
 
 
                     <div class="flex flex-col gap-3">
-                        <a href="http://localhost:8080/oauth2/authorization/google" class="flex items-center justify-center w-full px-4 py-3 bg-gradient-to-r from-white/10 to-white/5 border border-gold/30 rounded-xl hover:border-gold hover:shadow-[0_0_15px_rgba(255,184,0,0.3)] transition-all duration-300 group">
+                        <a href="https://spirituality-be-production.up.railway.app/oauth2/authorization/google" class="flex items-center justify-center w-full px-4 py-3 bg-gradient-to-r from-white/10 to-white/5 border border-gold/30 rounded-xl hover:border-gold hover:shadow-[0_0_15px_rgba(255,184,0,0.3)] transition-all duration-300 group">
                             <svg class="w-5 h-5 mr-3" viewBox="0 0 24 24">
                                 <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
                                 <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
